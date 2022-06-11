c255cdab5d62c3096decf664df30099510da9cbb
Ben <ben.a.burgos@gmail.com>
2022-06-11 11:58:41 -0700

    updates atom to a hook

--- src/atom.js
+++ src/atom.js
@@ -1,9 +1,13 @@
-import { atom } from 'recoil';
+import { atom, useRecoilState } from 'recoil';
 
 // declare and export an atom
-export const counterState = atom({
+const counterState = atom({
   // the key is used to track the state internally in recoil
   key: 'counterState',
   // default value is the value if no other value exists, the starting value
   default: 0,
 });
+
+export const useCounterState = () => {
+  return useRecoilState(counterState)
+}
